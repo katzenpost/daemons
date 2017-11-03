d53ac0d4776e2e6cecd7b184c8daaac2800843a1
Yawning Angel <yawning@schwanenlied.me>
2017-11-03 13:15:59 +0000

    server: Add the server daemon main executable bits.

---
+++ server/main.go
@@ -0,0 +1,72 @@
+// main.go - Katzenpost server binary.
+// Copyright (C) 2017  Yawning Angel.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
+
+	"github.com/katzenpost/server"
+	"github.com/katzenpost/server/config"
+)
+
+func main() {
+	cfgFile := flag.String("f", "katzenpost.toml", "Path to the server config file.")
+	flag.Parse()
+
+	// Set the umask to something "paranoid".
+	syscall.Umask(0077)
+
+	// Ensure that a sane number of OS threads is allowed.
+	if os.Getenv("GOMAXPROCS") == "" {
+		// But only if the user isn't trying to override it.
+		nProcs := runtime.GOMAXPROCS(0)
+		nCPU := runtime.NumCPU()
+		if nProcs < nCPU {
+			runtime.GOMAXPROCS(nCPU)
+		}
+	}
+
+	cfg, err := config.LoadFile(*cfgFile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to load config file '%v': %v\n", *cfgFile, err)
+		os.Exit(-1)
+	}
+
+	// Setup the signal handling.
+	ch := make(chan os.Signal)
+	signal.Notify(ch, os.Interrupt, syscall.SIGTERM)
+
+	// Start up the server.
+	svr, err := server.New(cfg)
+	if err != nil {
+		if err == server.ErrGenerateOnly {
+			os.Exit(0)
+		}
+		fmt.Fprintf(os.Stderr, "Failed to spawn server instance: %v\n", err)
+		os.Exit(-1)
+	}
+	defer svr.Shutdown()
+
+	// Wait till externally signaled termination, the server will not self
+	// terminate.
+	<-ch
+}
